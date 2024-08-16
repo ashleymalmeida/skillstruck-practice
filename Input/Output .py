d142401b3122d7db80c025d1724e88bea6007872
ashleymalmeida <almeida20082017@gmail.com>
2024-08-16 12:51:50 -0700

    exported from Skill Struck

---
+++ Input/Output .py
@@ -0,0 +1,7 @@
+#CHECKPOINT
+# favorite_color = input("What is your favorite color?")
+# print("Your favorite color is " + favorite_color)
+
+#Challenge
+# name = input(" What is your name?  ")
+# print( 'Hello, ' + name + '!')
