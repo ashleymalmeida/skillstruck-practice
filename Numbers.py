59a858c4ef58a420ad5354517ef8fb9ebec415a0
ashleymalmeida <almeida20082017@gmail.com>
2024-08-16 12:51:51 -0700

    exported from Skill Struck

---
+++ Numbers.py
@@ -0,0 +1,17 @@
+#CHEKPOINT
+# pumpkins = 20
+# scarecrows = 16
+
+# fish = 2.0
+# monkey = 5.3
+
+#Challenge 1
+# feet_in_meters = 4.1
+# print(feet_in_meters)
+
+average_pets = '13'
+print(average_pets)
+
+
+average_pets = 5.0*2.0
+print(average_pets)
