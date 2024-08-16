cb5e55d72664396cc5bdec1799af24e860585aee
ashleymalmeida <almeida20082017@gmail.com>
2024-08-16 12:51:52 -0700

    exported from Skill Struck

---
+++ Python_math.py
@@ -0,0 +1,15 @@
+#Challenge 1
+# money_per_person = int(input("How much did you ask each person to bring?"))
+
+# number_of_people = int(input("How many people are going?"))
+
+# equation = money_per_person*number_of_people
+# print(equation)
+
+
+# Challenge 2
+people = int(input("How many family members?"))
+inheritance = 48682.76
+money_per_person = inheritance / people
+
+print("If " + str(people) + " people split the inheritance, each person would get " + str(money_per_person) + " dollars.")
