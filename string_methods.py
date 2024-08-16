18793d2820efa9dad2e9b3d985dff3012bebc9de
ashleymalmeida <almeida20082017@gmail.com>
2024-08-16 12:51:51 -0700

    exported from Skill Struck

---
+++ string_methods.py
@@ -0,0 +1,13 @@
+# CHECKPOINT
+# string_1 = "I love learning!"
+# print(string_1.upper())
+# print(string_1.lower())
+# print(string_1.replace("leanring", "smiling"))
+
+# Challenge 1
+# cheer = input("How many dozen are In one order of wings?")
+# print(cheer.replace( "1" ,  "one" ))
+
+# Challenge 2
+# shout = input("How do you call your parents attention?")
+# print(shout.upper())
