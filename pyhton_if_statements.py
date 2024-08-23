d37581fe6de1803baa545eb3cd88017e121b40c3
ashleymalmeida <almeida20082017@gmail.com>
2024-08-23 12:11:24 -0700

    exported from Skill Struck

---
+++ pyhton_if_statements.py
@@ -0,0 +1,19 @@
+#CHECKPOINT
+# chickens = 3
+# geese = 7
+
+# if geese > chickens:
+    # print("The geese has more amount than the chickens")
+# else:
+    # print("The geese are a small group")
+
+
+odd = input("Enter an odd number")
+even = 
+
+if               :
+    print(      )
+else:
+    print(      )
+
+
