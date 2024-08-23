23b470cec317d6d33fd819fb4f477dba7a1b81ab
ashleymalmeida <almeida20082017@gmail.com>
2024-08-23 12:11:22 -0700

    exported from Skill Struck

---
+++ concatenating_numbers.py
@@ -0,0 +1,28 @@
+# CHECKPOINT
+# string = "The number of test we've recieved totals up to {}"
+# test = 30
+# print(string.format(test))
+
+
+
+# Challenge 1 
+# age = 12
+# int variable 
+# birthday = "February 13th, {}"
+# string variable
+
+# print(birthday.format(age))
+
+
+
+
+# Challenge 2
+# number = input("Pick a three number digits")
+# string variable
+# hours = int(int(number)/60) 
+# minutes = int(number) % 60
+
+
+# complete = " It has been {} hour(s) and {} minute(s) since midnight."
+# print(complete.format(hours, minutes))
+
