969628bc249e49e3c0f00d204ab40c24dc591a44
ashleymalmeida <almeida20082017@gmail.com>
2024-08-30 12:14:20 -0700

    exported from Skill Struck

---
+++ if_statements_in_a_loop.py
@@ -0,0 +1,19 @@
+# CHECKPOINT
+# foods = ["mushrooms", "fish", "broccoli", "potato soup"]
+
+
+# for x in foods :
+#     if x == "mushrooms" :
+#         print(x + " are gross.")
+#     elif x == "fish":
+#         print(x + " is gross.")
+#     elif x == "broccoli" :
+#         print(x + " is gross.")
+#     else:
+#         print("Does not caterogies above.")
+        
+
+
+
+
+    
