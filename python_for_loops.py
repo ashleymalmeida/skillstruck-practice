639f30f1e1b5b2a44b2c702d2ca5ed40d481b296
ashleymalmeida <almeida20082017@gmail.com>
2024-08-30 12:14:20 -0700

    exported from Skill Struck

---
+++ python_for_loops.py
@@ -0,0 +1,24 @@
+# Checkpoint 
+# animals = ["lion", "elephant", "giraffe" ,   "cheetah"] 
+
+# for x in animals:
+#     print(x + " is the spikiest animal ever!"  )
+
+
+# Challenge 1
+# var1 = int(input("Insert a number"))
+# var2 = int(input("Insert a number"))
+
+# for x in range(var1, var2 + 1 ):
+#     print(x)
+
+
+
+# Challenge 2 
+# rg1 = int(input("Insert a number"))
+# rg2 = int(input("Insert a number"))
+# total = 0 
+
+# for x in range( rg1, rg2   ):
+#     total = total + x
+# print(total)
