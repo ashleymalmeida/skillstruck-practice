aa4e1f7d4e8e6937cbc53db4b0bb7004ab66672d
ashleymalmeida <almeida20082017@gmail.com>
2024-09-06 13:29:36 -0700

    exported from Skill Struck

---
+++ python_lists_continued.py
@@ -0,0 +1,38 @@
+# CHECKPOINT
+# activities = ["movies", "skating", "bowling", "laser tag", "escape room", "trampoline park", "library"]
+
+# print(activities[0:3])
+
+# activities[3] = "water park"
+
+# print(activities)
+
+# print(len(activities))
+
+
+
+# Challenge 1
+# my_list = [int(n) for n in input().split()]
+# biggest = 0
+# for x in my_list:
+# 	if x > biggest :
+# 		biggest = x
+
+# print(biggest)
+
+# Another example for challenge 1
+# # my_list.sort(reverse=True)
+# # print(my_list[0])
+
+
+# Challenge 2 
+# my_list = [int(n) for n in input().split()]
+
+# current = my_list[0]
+
+# for x in my_list :
+#     if x > current:
+#         print(x)
+#     current = x
+
+
