56dd2c80275fd71f8dc5d807122b3353e0e342d6
ashleymalmeida <almeida20082017@gmail.com>
2024-09-06 13:29:33 -0700

    exported from Skill Struck

---
+++ python_accessing_items_in_a_list.py
@@ -0,0 +1,20 @@
+# CHECKPOINT
+# sports = ["basketball", "dance", "soccer", "ping pong", "volleyball"]
+
+# print(sports[2])
+
+# print("I love to play  " +  sports[2] )
+
+
+
+# Challenge 1
+# my_list = [int(n) for n in input("Create a list of 9 numbers").split()]
+
+# print(my_list[4] * 100)
+
+
+# Challenge 2
+# my_list = input("Insert 5 words").split()
+
+# print("Some animals " + my_list[0]  + " and some " + my_list[4] )
+
