b09935cb1c809523471213e587382c8f7a0feba9
ashleymalmeida <almeida20082017@gmail.com>
2024-09-06 13:29:38 -0700

    exported from Skill Struck

---
+++ python_functions.py
@@ -0,0 +1,26 @@
+# Checkpoint
+# def race ():
+#     print("The person who won the race is peter!")
+    
+
+# race()
+
+
+# Challenge 1 
+# def charlie ():
+#     print("Charlie is the best dog in the whole world!")
+    
+
+# charlie()
+
+
+# Challenge 2 
+# start = input("Would you like to launch the game? enter yes or no")
+
+# def start_game():
+#     if start == "yes"  :
+#         print("Initialization Complete")
+#     else:
+#         print("Initialization Failed")
+
+# start_game()
