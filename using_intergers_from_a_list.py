30ce9e7ac5025a40d3945f84afc090e34c81dc4f
ashleymalmeida <almeida20082017@gmail.com>
2024-09-06 13:29:34 -0700

    exported from Skill Struck

---
+++ using_intergers_from_a_list.py
@@ -0,0 +1,31 @@
+# CHECKPOINT
+# lasers = [3, 10, 4, 15, 11]
+# equation = lasers[2] * 10
+
+# print(round(3.14159265, 3 ))
+
+# for x in range(0, 10):
+#     print(x)
+
+# for x in range(0, 10, 2):
+#     print(x)
+
+
+
+# Challenge 1 
+# my_list = [int(n) for n in input().split()]
+
+# sum1 = 0 
+# for x in my_list:
+#     sum1 = sum1 + x
+# print(sum1)
+
+
+
+#  Challenge 2
+# my_list = [int(n) for n in input().split()]
+
+# product1 = 1
+# for x in my_list:
+#     product1 = product1 * x
+# print(product1)
