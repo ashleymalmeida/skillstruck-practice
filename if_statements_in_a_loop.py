0f8e1d18a54a47774efca200470583a47debffda
ashleymalmeida <almeida20082017@gmail.com>
2024-09-06 13:29:33 -0700

    exported from Skill Struck

--- if_statements_in_a_loop.py
+++ if_statements_in_a_loop.py
@@ -13,7 +13,23 @@
 #         print("Does not caterogies above.")
         
 
+# Challenge 1
+# my_list = [int(n) for n in input("Input a list of numbers").split()]
 
+# num_of_zeros = 0
 
+# for number in my_list:
+#     if number == 0 : 
+#         num_of_zeros = num_of_zeros + 1
+# print(num_of_zeros)
 
+# Challenge 2 
+# my_list = [int(n) for n in input().split()]
+
+# #even_numbers = 0
+
+# for numbers in my_list :
     
+#     if numbers % 2 == 0: 
+#         #even_numbers = even_numbers + 1 
+#         print(numbers)
