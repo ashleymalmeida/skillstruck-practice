dedc202b81d6ac434c095b7a39ac5ba0db1f72bd
ashleymalmeida <almeida20082017@gmail.com>
2024-09-06 13:29:35 -0700

    exported from Skill Struck

---
+++ adding_to_lists_in_python.py
@@ -0,0 +1,27 @@
+# CHECKPOINT
+# flowers = ["rose", "tulip", "lilac", "sunflower"]
+
+# flowers.append("Dahlia")
+
+# print(flowers)
+
+
+
+# Challenge 1
+# first = input("What other item do you want?")
+# second = input("What final item do you want?")
+
+# treats = ["popcorn", "popsicles", "soda", "chips", "cookies"]
+
+
+# treats.extend([first, second])
+
+# print(treats)
+
+
+# Challenge 2
+# olympics = ["running", "gymnastics", "swimming", "volleyball", "basketball"]
+
+# olympics.extend(["karate", "surfing", "baseball", "skateboarding", "sport climbing"])
+
+# print(olympics)
