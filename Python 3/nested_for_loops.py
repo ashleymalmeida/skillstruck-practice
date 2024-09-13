e33408a5a07fc8cd5dff1a7f44dfc6d00dee5798
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:07:04 -0700

    exported from Skill Struck

---
+++ Python 3/nested_for_loops.py
@@ -0,0 +1,21 @@
+# CHECKPOINT
+# rows = 3
+# pets = ["dog", "cat", "parrot", "hamster", "rabbit"]
+# my_pets = [[n for n in pets] for i in range(rows)]
+
+# print(my_pets)
+
+# Challenge 1
+# rows = int(input("How many rows do you want?"))
+# mylist = [1, 2, 3, 4, 5]
+# list = [[(j*rows) for j in mylist] for i in range(rows)]
+
+# print(list)
+
+
+# Challenge 2
+# rows = input("Input a list of weather").split()
+# cols = ["windy", "breezy", "calm"]
+# list = [[(i + " " + j) for j in cols ] for i in rows]
+
+# print(list)
