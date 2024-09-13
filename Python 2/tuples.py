fec25114bc0edd53fa2320a5fa3df19d65601345
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:52 -0700

    exported from Skill Struck

---
+++ Python 2/tuples.py
@@ -0,0 +1,22 @@
+# CHECKPOINT
+# grades = (95, 70, 85, 92, 100)
+
+# print(grades)
+# print(grades[-2])
+# print(grades[0:3])
+
+# Challenge 1
+# friends = (3, 5, 7, 8, 10, 2, 50, 4)
+# guess = int(input("Pick a number"))
+
+# if guess in friends:
+#     print("Yes")
+# else:
+#     print("No")
+
+# Challenge 2
+# friends = ("Bob", "Zoe", "Tim", "Kat", "Sam", "Jim", "Jon", "Kim")
+
+# print(friends[-3])
+# print(friends[5])
+# print(friends[0:4])
