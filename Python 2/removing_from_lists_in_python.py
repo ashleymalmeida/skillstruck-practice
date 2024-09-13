0ec4b61944d0075c6571959a17f8c6adf204f888
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:44 -0700

    exported from Skill Struck

---
+++ Python 2/removing_from_lists_in_python.py
@@ -0,0 +1,24 @@
+# CHECKPOINT
+# harvest = ["pumpkins", "apples", "corn", "squash", "carrots"]
+
+# harvest.remove("squash")
+
+# apple = harvest.pop(1)
+
+# print(harvest)
+
+
+# Challenge 1 
+# choice = input("What skill do you want to remove?")
+
+# skills = ["Piano", "Soccer", "Coding", "Cooking", "Writing"]
+
+# skills.remove(choice)
+
+# print(skills)
+
+
+# Challenge 2
+# my_list = input("Insert 5 names").split()
+
+# print(my_list.pop(2) + " got the bronze medal")
