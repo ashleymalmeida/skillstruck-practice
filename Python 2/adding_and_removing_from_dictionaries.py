c0d0afc94ac94dcab2e51e8dd45df47f0db7219d
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:49 -0700

    exported from Skill Struck

---
+++ Python 2/adding_and_removing_from_dictionaries.py
@@ -0,0 +1,43 @@
+# Checkpoint
+# friends = {
+# 	"Shane" : 10,
+# 	"Samantha" : 9,
+# 	"Shiloh" : 12,
+# 	"Sean" : 11	
+# }
+
+# friends["Sebastian"] = 8
+# print(friends)
+# friends.pop("Shiloh")
+# print(friends)
+
+
+# Challenge 1 
+# my_shape = input("What shape do you want to add?")
+# my_shape_height = int(input("How tall is your shape?"))
+
+# shapes = {
+#     "Triangle": 8,
+#     "Circle": 15,
+#     "Square": 10,
+# 	"Rectangle" : 12,
+# }
+
+# shapes[my_shape] = my_shape_height
+
+# print(shapes)
+
+
+# Challenge 2
+# name = input("Which tree do you want to remove?")
+# trees = {
+# 	"aspen" : 75,
+# 	"pine" : 82,
+# 	"maple" : 60,
+# 	"oak" : 65,
+# 	"willow" : 80,
+# 	"cottonwood" : 100,	
+# }
+
+# trees.pop(name)
+# print(trees)
