4155b15a421ab177c3bcef2fbfd588e4be08853f
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:53 -0700

    exported from Skill Struck

---
+++ Python 3/reading_files.py
@@ -0,0 +1,23 @@
+# Testing
+# file = open("example.txt", "r")
+# print(file.read))
+
+# file = open("example.txt", "r")
+# print(file.read))
+# file.close()
+
+
+
+
+# CHECKPOINT
+# file = open("example.txt", "r")
+# print(file.read())
+
+# Challenge 1
+# file = open("cobra.txt",  "r")
+# print(file.read())
+
+# Challenge 2 
+# file = open("speech.txt", "r")
+# print(file.read())
+
