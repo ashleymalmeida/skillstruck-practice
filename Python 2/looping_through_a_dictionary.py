a5819ed7c236ac47703e1f8224c1290fb6fe1ef4
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:50 -0700

    exported from Skill Struck

---
+++ Python 2/looping_through_a_dictionary.py
@@ -0,0 +1,42 @@
+# CHECKPOINT
+# measurement = {"length": 10, "width": 5, "depth": 3}
+
+# for x in measurement.values():
+# 	print(x)
+
+# Challenge 1
+# amanda_value = int(input("How many in Amanda's group?"))
+# jane_value = int(input("How many in Jane's group?"))
+
+
+# group = {
+# 	"Fred" : 12,
+# 	"Jackson" : 15,
+# 	"Sophie" : 20,
+# 	"Amanda" : amanda_value,
+# 	"Jane" : jane_value,
+# }
+
+# total = 0
+
+# for x in group.values():
+# 	total = total + x
+	
+# print(total)
+
+# Challenge 2
+# first = int(input("Pick a first number"))
+# second = int(input("Pick a second number"))			
+# group = {
+# 	3 : 10,
+# 	5 : 3,
+# 	10 : 6,
+# 	4 : 30,
+# 	first : second
+# }
+
+# total = 0
+# for x, y in group.items() :
+# 	total += x * y
+
+# print(total)
