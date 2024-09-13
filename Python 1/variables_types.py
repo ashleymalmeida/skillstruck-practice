44aabde8524b538bc27d304d186f4dce90f192a0
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:31 -0700

    exported from Skill Struck

---
+++ Python 1/variables_types.py
@@ -0,0 +1,35 @@
+#CHECKPOINT
+# name = 'lily'
+# print(name)
+# age = 13
+# print(age)
+# happy = 'True'
+# print(happy)
+
+# shown = False
+# shown2 = "False"
+# print(shown)
+# print(shown2)
+
+# Challenge 1 
+# student1 = True
+# student2 = "True"
+# print(student1)
+# print(student2)
+
+
+# clowns1 = False
+# clowns2 = "False"
+# print(clowns1)
+# print(clowns2)
+
+#Challenge 2
+# neighbor = "True"
+# greeting = "Hello!"
+# number = 42
+
+# print(neighbor)
+# print(greeting)
+# print(number)
+
+
