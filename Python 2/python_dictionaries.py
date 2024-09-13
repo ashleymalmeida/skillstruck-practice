1dfd61a2520352279aaad6019a4a8f875b5b337c
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:48 -0700

    exported from Skill Struck

---
+++ Python 2/python_dictionaries.py
@@ -0,0 +1,42 @@
+# Checkpoint
+# mountains = {"Timpanogos" : 5, "Everest": 10, "Kilimanjaro": 6, "Vesuvius": 7}
+
+# print(mountains)
+# print(mountains["Vesuvius"])
+
+# mountains["Kilimanjaro"] = 12
+
+# print(mountains)
+
+
+# Challenge 1
+# holidays = {
+#     "January" : "New Years",
+#     "February" : "Valentine's Day",
+#     "December" : "Christmas",
+#     "April" : "Easter",
+#     "October" : "Halloween",
+#     "November" : "Thanksgiving",
+#     "July" : "4th of July",
+#     "September" : "Labor Day",
+#     "May" : "Memorial Day",
+#     "June" : "Juneteenth National Independence Day",
+#  }
+
+# print(holidays)
+
+
+# Challenge 2
+# packing = {
+#     "shoes": "Nike" , 
+#     "socks": "White", 
+#     "shirts": "Abrecombie", 
+#     "pants": "Levis", 
+#     "pjs": "Panda Onesie"
+#   }
+
+# print(packing)
+
+# packing_list = ["shoes", "socks", "shirts", "pants", "pjs"]
+
+# print(packing_list)
