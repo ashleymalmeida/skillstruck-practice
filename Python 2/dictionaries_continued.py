6ffa85e6278fc69fbe60aaeb04d87f08783eb13c
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:50 -0700

    exported from Skill Struck

---
+++ Python 2/dictionaries_continued.py
@@ -0,0 +1,51 @@
+# Checkpoint
+# coins = {
+#    		 "pennies" : 1,
+#    		 "nickels": 5,
+#   		 "dimes": 10,
+#   		 "quarters": 25,
+# }
+
+# print(coins)
+# coins["silver dollar"] = 15
+# coins.pop("pennies")
+# print(coins)
+# print(len(coins))
+
+
+# Challenge 1 
+# dictionary = {
+# 	1: "bicycle",
+# 	2: "soccer balls",
+# 	3: "piano books",
+#     4: "",
+#     5: "",
+#     6: "",
+# }
+
+# dictionary[4] = input("What do you have 4 of?")
+# dictionary[5] = input("What do you have 5 of?")
+# dictionary[6] = input("What do you have 6 of?")
+
+# print(dictionary)
+
+
+# Challenge 2
+# work = {
+
+# 	"Monday": 3,
+# 	"Tuesday": 4, 
+# 	"Wednesday": 2,
+# 	"Thursday": 1, 
+# 	"Friday": 1, 
+	
+# }
+
+# work["Saturday"] = 5
+# work.pop("Wednesday")
+# print(len(work))
+
+# if "Friday" in work:
+# 	print("Friday is in work")
+
+# print(work)
