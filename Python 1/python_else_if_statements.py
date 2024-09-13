259a0cc684c09d5bdbaae90297161183daf3026d
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:39 -0700

    exported from Skill Struck

---
+++ Python 1/python_else_if_statements.py
@@ -0,0 +1,34 @@
+# Checkpoint
+# coins = 10
+
+# if coins > 20:
+# 	print("You have more than enough to buy a puppy")
+# elif coins == 20:
+# 	print("You have excatly enough to buy a puppy")
+# else: 
+# 	print("You do not have enough to buy a puppy")
+
+
+# Challenge 1
+# num1 = input("Insert a number a number between 0-10")
+# num2 = input("Insert a number a number between 0-10")
+# num3 = input("Insert a number a number between 0-10")
+
+# if num1 <= num2 and num1 <= num3 :
+#     print(num1)
+# elif num2 <= num1 and num2<=num3 :
+#     print(num2)
+# else:
+#     print(num3)
+
+# Checkpoint 2
+# number1 = input("Insert a number")
+# number2 = input("Insert a number")
+# number3 = input("Insert a number")
+
+# if number1 == number2 and number2 == number3 and number1 == number3 :
+#     print("3")
+# elif number1 < number2 and number2 < number3 and number1 < number3  :
+#     print("0")
+# else:
+#    print("2")
