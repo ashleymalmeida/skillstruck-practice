ef621ec1910f8273868fc5b47ff33c3d9a274bdf
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:35 -0700

    exported from Skill Struck

---
+++ Python 1/checking_strings.py
@@ -0,0 +1,20 @@
+# CHECKPOINT
+#  string1 = "I would like a chocolate cake!"
+#  check = "a" in string
+#  print(check)
+
+#  outputs "True"
+
+
+
+# Challenge 1
+# string1 = input("What would you love to have?")
+# check = "armadillo" in string1
+# print(check)
+
+# outputs "True"
+
+
+
+
+
