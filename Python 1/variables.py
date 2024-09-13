6c3c26ecf4e7c45fd6be4cbd4c8be28edd1c4fd6
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:30 -0700

    exported from Skill Struck

---
+++ Python 1/variables.py
@@ -0,0 +1,34 @@
+#CHECKPOINT
+neighbor = "Bob"
+age = 42
+
+print(neighbor)
+print(age)
+
+
+my_bff = "Lexi"
+age = 16
+
+print(my_bff)
+print(age)
+
+
+my_pet = "Angel"
+age = 2
+
+print(my_pet)
+print(age)
+
+
+#CHECKPOINT 1
+how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood = 34
+print(how_much_wood_would_a_wood_chuck_chuck_if_a_wood_chuck_could_chuck_wood)
+
+main_course = "Chicken"
+print(main_course)
+
+side = "Mash potatoes"
+print(side)
+
+drink = "Milk"
+print(drink)
