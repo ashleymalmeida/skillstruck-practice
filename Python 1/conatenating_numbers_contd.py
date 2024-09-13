85e8a59638462b24f8c81ad7d4548eaf4f8fe06a
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:36 -0700

    exported from Skill Struck

---
+++ Python 1/conatenating_numbers_contd.py
@@ -0,0 +1,26 @@
+#CHECKPOINT
+# string = "The total number of layers we made of cake was {}. Wow {} is a huge amount!"
+# quantity = 5
+# category = 5
+# print(string.format(quantity, category))
+
+
+
+#Challenge 1
+# string = float(input("Insert a long number"))
+# equation = string * 10
+# second_step = int(equation % 10)
+# sentence = "The first decimal digit is {}"
+# print(sentence.format(second_step))
+
+
+
+#Challenge 2
+# dollars = int(input("What are the whole dollars"))
+# cents = int(input("How many cents in total"))
+# question = int(input("How many cookies do you want to buy?"))
+# cents2 = cents * 0.01
+# total = (dollars + cents2) * question
+# message = "The total cost of {} cookies is ${}"
+
+# print(message.format(question, total))
