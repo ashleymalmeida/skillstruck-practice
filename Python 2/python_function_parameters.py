563749f423d6d62d2bcf40ac9b262e45fd6f62aa
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:47 -0700

    exported from Skill Struck

---
+++ Python 2/python_function_parameters.py
@@ -0,0 +1,30 @@
+# Checkpoint
+# def practice(activity):
+# 	print("I like to practice " + activity )
+	
+	
+# practice("soccer")
+# practice("volleyball")
+# practice("basketball")
+
+
+
+# Challenge 1 
+# fruit = input("Please enter the name of a fruit: ")
+
+# def hungryForApples(choice):
+#     print(choice + choice + choice)
+	
+# hungryForApples(fruit)
+
+
+
+# Challenge 2
+# answer = int(input("What is the radius of the circle?"))
+
+# def circle (radius) :
+# 	answer = 3.14159 * radius ** 2
+# 	print(round(answer, 1))
+	
+
+# circle(answer)
