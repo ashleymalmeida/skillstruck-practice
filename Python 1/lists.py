5f044229225a583a7d1fb97d340bfefcfd9dcb04
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:37 -0700

    exported from Skill Struck

---
+++ Python 1/lists.py
@@ -0,0 +1,21 @@
+#CHECKPOINT
+# candy = [ "Hershey", "Gummy Bears", "Twix", "Gum"    ]
+# pieces = [ 23 , 14 , 33 , 35 ]
+
+# print(candy)
+# print(pieces)
+
+
+
+#Challenge 1
+# friends = ["Audomar", "Alondra", "Maddie"]
+# print(friends)
+
+
+#Challenge 2
+# reasons = ["opportunities", "high education", "gain skills" ]
+# print(reasons)
+
+
+
+
