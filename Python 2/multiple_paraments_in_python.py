1af38972a249439e213371232bfc69b90e1a1c47
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:48 -0700

    exported from Skill Struck

---
+++ Python 2/multiple_paraments_in_python.py
@@ -0,0 +1,34 @@
+# Checkpoint
+# def name (first, last) :
+# 	print("The first name is " + first )
+# 	print("The last name is " + last )
+	
+
+# name("Ashley", "Almeida")
+# name("Marina", "Almeida")
+
+
+
+# Challenge 1
+# fruit1 = input("Please enter the name of the fruit you would like: ")
+# fruit2 = input("Please enter the name of the fruit you would like: ")
+# fruit3 = input("Please enter the name of the fruit you would like: ")
+
+# def hungryForApples(fruit1,fruit2,fruit3):
+#     print(fruit1 + fruit2 + fruit3 )
+
+# hungryForApples(fruit1, fruit2, fruit3 )
+
+
+# Challenge 2
+# choice1 = int(input("What is the first number?"))
+# choice2 = int(input("What is the second number?"))
+
+# def my_function(first, second):
+# 	if first < second :
+# 		print(first)
+# 	else:
+# 		print(second)
+		
+
+# my_function(choice1, choice2)
