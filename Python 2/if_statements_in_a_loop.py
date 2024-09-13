14b885c434cb3765f23e01b9b9b1912050cdc3a4
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:41 -0700

    exported from Skill Struck

---
+++ Python 2/if_statements_in_a_loop.py
@@ -0,0 +1,35 @@
+# CHECKPOINT
+# foods = ["mushrooms", "fish", "broccoli", "potato soup"]
+
+
+# for x in foods :
+#     if x == "mushrooms" :
+#         print(x + " are gross.")
+#     elif x == "fish":
+#         print(x + " is gross.")
+#     elif x == "broccoli" :
+#         print(x + " is gross.")
+#     else:
+#         print("Does not caterogies above.")
+        
+
+# Challenge 1
+# my_list = [int(n) for n in input("Input a list of numbers").split()]
+
+# num_of_zeros = 0
+
+# for number in my_list:
+#     if number == 0 : 
+#         num_of_zeros = num_of_zeros + 1
+# print(num_of_zeros)
+
+# Challenge 2 
+# my_list = [int(n) for n in input().split()]
+
+# #even_numbers = 0
+
+# for numbers in my_list :
+    
+#     if numbers % 2 == 0: 
+#         #even_numbers = even_numbers + 1 
+#         print(numbers)
