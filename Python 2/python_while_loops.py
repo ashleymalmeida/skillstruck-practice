45e6aa3288e8204fda851b2912963502fe64e172
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:45 -0700

    exported from Skill Struck

---
+++ Python 2/python_while_loops.py
@@ -0,0 +1,22 @@
+# CHECKPOINT
+# number = 1
+
+# while number <= 50 :
+# 	print(number)
+# 	number += 1
+
+# Challenge 1
+# total = 0
+# number = int(input("Enter a number"))
+# while number > 0 :
+#     number = int(input("Enter a number or 0 to stop"))
+#     total += 1
+# print(total)
+
+# Challenge 2
+# my_number = int(input("Pick a number."))
+# total = 0
+# while my_number > 0 :
+#     total += my_number
+#     my_number = int(input("Pick a number or 0 to stop."))
+# print(total)
