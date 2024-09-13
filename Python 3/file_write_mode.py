90cf804f5738aa87406f7507153a8995825aa761
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:07:01 -0700

    exported from Skill Struck

---
+++ Python 3/file_write_mode.py
@@ -0,0 +1,16 @@
+# CHECKPOINT
+# file = open("porcupine.txt", "w")
+# file.write("In short, I love to code!")
+# file.close()
+
+# Challenge 1
+# file = open("email.txt", "w")
+# file.write("Never mind")
+# file.close()
+
+# Challenge 2
+# answer = input("What does a file do?")
+# file.write(answer)
+
+# file = open("report.txt", "w")
+# print(file.read())
