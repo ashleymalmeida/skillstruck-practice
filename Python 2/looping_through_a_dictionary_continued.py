22d4a410f806adb05d4089a8ac45445e34a7c455
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:51 -0700

    exported from Skill Struck

---
+++ Python 2/looping_through_a_dictionary_continued.py
@@ -0,0 +1,57 @@
+# CHECKPOINT
+# ride = {
+#     "William" : 120,
+#     "Estacy" : 4,
+#     "Asher" : 8,
+#     "Amelia" : 2,
+#     "Jemuel" : 9,
+
+# }
+
+
+# for x in ride.values():
+#     if x >= 100:
+
+#         print("This person is tall enough to ride")
+        
+#     else:
+#         print("This person is too short to ride")
+
+
+# Challenge 1
+# dictionary = {
+#     7: "first",  
+#     3: "second",
+#     4: "third",
+#     8: "fourth",
+#     9: "fifth",
+# }
+
+# my_list = [int(n) for n in input().split()]
+
+# for number in my_list:
+#     if number in dictionary:
+#         print("Yes")
+#     else:
+#         print("No")
+
+
+# Challenge 2 
+# words_count = {}
+# words = input("Create a list of words").split()
+# for word in words:
+#     if word not in words_count:
+#         words_count[word] = 0
+#     words_count[word] += 1
+
+
+
+# biggest = 0
+
+# for x in words_count.values():
+#     if x > biggest:
+#         biggest = x
+
+# for key in words_count.keys():
+#     if words_count[key] == biggest:
+#         print(key)
