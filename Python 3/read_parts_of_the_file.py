7d5445453afce75bf7e8aa099200bbc015591c07
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:56 -0700

    exported from Skill Struck

---
+++ Python 3/read_parts_of_the_file.py
@@ -0,0 +1,18 @@
+# CHECKPOINT
+# file = open("testing.txt", "r")
+# print(file.read(10))
+# print(file.readline())
+# print(file.readline())
+# file.close()
+
+# Challenge 1
+# number = int(input("How many characters do you want to print?"))
+# file = open("words.txt", "r")
+# print(file.read(number))
+# file.close()
+
+# Challenge 2
+# file = open("reading.txt", "r")
+# print(len(file.readlines()))
+# file.close()
+
