f5acb89fdb57ccb8073f9c28e35ee2106cfb2a12
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:38 -0700

    exported from Skill Struck

---
+++ Python 1/pyhton_if_statements.py
@@ -0,0 +1,31 @@
+#CHECKPOINT
+# chickens = 3
+# geese = 7
+
+# if geese > chickens:
+    # print("The geese has more amount than the chickens")
+# else:
+    # print("The geese are a small group")
+
+
+# Challenge 1 
+# question = int(input("Enter a three digit number"))
+
+# # if  100 <= question < 1000:
+#     print("Yes")
+# else:
+#     print("No")
+
+
+
+
+
+
+# Challenge 2
+# number  = 13
+
+# if number % 3 == 0 :
+#     print("Is divisible by 3")
+    
+# else:
+#     print("Is not divisible by 3")
