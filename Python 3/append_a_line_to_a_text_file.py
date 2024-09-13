3c76c70e855442159cbe0caffe42160b9775db52
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:59 -0700

    exported from Skill Struck

---
+++ Python 3/append_a_line_to_a_text_file.py
@@ -0,0 +1,24 @@
+# CHECKPOINT
+# file = open("adding.txt", "a")
+# file.write("I love programming!")
+# file.close()
+
+# file = open("adding.txt", "a")
+# print(file.read())
+
+# Challenge 1
+# file = open("letter.txt", "a")
+# file.write("From your Pen Pal")
+# file.close()
+
+# file = open("letter.txt", "a")
+# print(file.read())
+
+
+# Challenge 2 
+# file = open("report.txt", "a")
+# file.write("Quote was said by Gandhi")
+# file.close()
+
+# file = open("report.txt", "a")
+# print(file.read())
