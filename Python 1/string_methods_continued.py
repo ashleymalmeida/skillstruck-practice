e83c18e0bbe8ec953badc9982c5331255336781f
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:34 -0700

    exported from Skill Struck

---
+++ Python 1/string_methods_continued.py
@@ -0,0 +1,46 @@
+#CHECKPOINT
+# string1 = "Coding is amazing!"
+# print(string1.split())
+
+# string1 = "Coding is interesting!"
+# print(len(string1))
+
+# string1 = "Coding is crazy fun!"
+# print(string1.find("s"))
+
+# string1 = "I love computers!"
+# print(string1[4:9])
+
+
+#Challenge 1 
+# string1 = input("Type a sentence")
+# new_string1 = string1.split()
+# print(len(new_string1))
+
+
+
+
+# 
+# third = string1[1:3]
+# print(third)
+
+# last = string1[11:]
+# print(last)
+
+# fourth = string1[:4]
+
+string1 = input("What do you want?")
+
+
+
+third = string1[2]
+# print(third)
+
+last = string1[-3]
+# print(last)
+
+fourth = string1[3]
+# print(fourth)
+
+# print(third + last + fourth)
+
