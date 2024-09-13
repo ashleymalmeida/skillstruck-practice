4a2cacb6b99aceb0e278651f14add759e614f561
ashleymalmeida <almeida20082017@gmail.com>
2024-09-13 12:06:39 -0700

    exported from Skill Struck

---
+++ Python 1/python_conditionals.py
@@ -0,0 +1,28 @@
+# Checkpoint
+# question = input("How old are you?")
+# license = True 
+
+
+# if int(question) >= 16 and license == True :
+# 	print("You are old enough to drive.")
+# else:
+# 	print("You are not able to drive.")
+
+
+#Challenge 1 
+# height = input("How tall are you in inches?")
+# check = True
+
+# if int(height) >= 48 and check == True :
+#     print("You can ride the roller coaster.")
+# else:
+#    print("You can't ride the roller coaster." )
+
+
+# Challenge 2
+# number = input("Insert a two number digit between 0 and 100")
+
+# if int(number) <= 50 :
+#     print("Fewer than average" )
+# else:
+#    print("More than average" )
