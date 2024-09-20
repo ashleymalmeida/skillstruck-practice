c3b6fd369f9970f53ff2889841ff3c504f52f561
ashleymalmeida <almeida20082017@gmail.com>
2024-09-20 12:33:35 -0700

    exported from Skill Struck

--- Python 3/multi_dimensional_lists.py
+++ Python 3/multi_dimensional_lists.py
@@ -8,3 +8,25 @@
 #         col.append(5)
 #     list.append(col)
 # print(list)
+
+# Challenge 1 
+# rows = ["Alex", "Angel", "Chris", "Layla"]
+# cols = ["Roberts", "Fernandez", "Rodriguez", "Castro"]
+# list = []
+# for i in rows:
+#     col = []
+#     for j in cols:
+#         col.append(i + " " + j)
+#     list.append(col)
+# print(col)
+
+# Challenge 2
+# flavors = ["apple", "grape", "peach", "cinnamon", "vanilla"]
+# rows = input("Input a list of fruits").split()
+# combination = []
+# for fruit in rows:
+#     col = []
+#     for flavor in flavors:
+#         col.append(fruit + " " + flavor)
+#     combination.append(col)
+# print(combination)
