5779d7a6ff96d8369b3692dc9d927cb94e5d24ff
ashleymalmeida <almeida20082017@gmail.com>
2024-09-20 12:33:37 -0700

    exported from Skill Struck

---
+++ Python 3/object_oriented_programming.py
@@ -0,0 +1,25 @@
+# CHECKPOINT
+# class Dog:
+#   	def __init__(self, name, color, size):
+#           self.name = name
+#           self.color = color
+#           self.size = size
+
+# Challenge 1
+# class Car:
+#   	def __init__(self, kind, color, year):
+#           self.name = name
+#           self.kind = kind
+#           self.color = color
+#           self.year = year
+
+# Challenge 2 
+# class Tree:
+#   	def __init__(self, fruit, height, leaf, flower, seed):
+#           self.name = name
+#           self.fruit = fruit
+#           self.height = height
+#           self.leaf = leaf
+#           self.flower = flower
+#           self.seed = seed
+
