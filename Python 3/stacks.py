7e1014ce913eb73dee22ccedd99a576b49953671
ashleymalmeida <almeida20082017@gmail.com>
2024-09-20 12:33:39 -0700

    exported from Skill Struck

---
+++ Python 3/stacks.py
@@ -0,0 +1,40 @@
+# CHECKPOINT
+# desserts = []
+
+# desserts.append(' ')
+# desserts.append(' ')
+# desserts.append(' ')
+# desserts.append(' ')
+
+# desserts.pop()
+
+# print(desserts)
+
+
+# Challenge 1 
+# first = "r"
+# second = "t"
+# third = "s"
+# fourth = "y"
+# fifth = "o"
+
+# answer = []
+
+# answer.append(third)
+# answer.append(second)
+# answer.append(fifth)
+# answer.append(first)
+# answer.append(fourth)
+
+# print(answer)
+
+# Challenge 2
+# response = input("What food should we bring?")
+				 
+# answer = ["apples", "steak", "potatoes", "carrots"]
+
+# if "s" in response:
+#     answer.append(response)
+#     print(answer)
+# else :
+#     print("The input doesn't have the letter s")
