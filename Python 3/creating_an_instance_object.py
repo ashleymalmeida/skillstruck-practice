daf42475d0f8795f5bd829ad4ca0a47da62c8452
ashleymalmeida <almeida20082017@gmail.com>
2024-09-20 12:33:37 -0700

    exported from Skill Struck

---
+++ Python 3/creating_an_instance_object.py
@@ -0,0 +1,46 @@
+# CHECKPOINT
+# class Hat:
+#   	def __init__(self, kind, color, material):
+#           self.name = name
+#           self.kind = kind
+#           self.color = color
+#           self.material = material
+	  
+# myObject = Hat("Hurley", "Waterproof", "Black", "Cotton")
+
+# print(myObject)
+
+
+# Challenge 1
+# class Fruit:
+
+#     def __init__(self, shape, kind, size):
+#         self.shape = shape 
+#         self.kind = kind 
+#         self.size = size
+
+# f1 = Fruit("Round", "Mango", "Medium")
+# f2 = Fruit("Oval", "Grapes", "Small")
+# f3 = Fruit("Oblong", "Banana", "Large")
+# f4 = Fruit("Round", "Orange", "Medium")
+
+# print(f1.kind)
+# print(f2.kind)
+# print(f3.kind)
+# print(f4.kind)
+
+# Challenge 2 
+# class Pet:
+   
+#    def __init__(self, name, kind, age):
+#          self.name = name
+#          self.kind = kind 
+#          self.age = age
+
+# p1 = Pet("Billy", "Cat", "1")
+# p2 = Pet("Willy", "Dog", "10")
+# p3 = Pet("Rosaline", "Rabbit", "4")
+
+# print(p1)
+# print(p2)
+# print(p3)
