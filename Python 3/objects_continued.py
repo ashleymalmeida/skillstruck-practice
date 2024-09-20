01100f243f82a80e174e685094c5a4808f46cb99
ashleymalmeida <almeida20082017@gmail.com>
2024-09-20 12:33:38 -0700

    exported from Skill Struck

---
+++ Python 3/objects_continued.py
@@ -0,0 +1,59 @@
+# CHECKPOINT
+# class Vacation:
+# 	def __init__(self, place, distance, weather):
+# 		self.place = place 
+# 		self.distance = distance 
+# 		self.weather = weather
+# def Tuesday(self):
+#         print("We will be hiking on Tuesday.")
+       
+       
+# summer = Vacation("Hawaii", 2000, "Sunny")
+
+# summer.rating = 10
+
+# summer.weather = "rainy"
+
+
+# print(summer)
+# print(summer.rating)
+# print(summer.weather)
+
+
+# Challenge 1 
+# class Friday:
+   
+# 	def __init__(self, activity, friend):
+# 		self.activity = activity
+# 		self.friend = friend
+		
+# 	def pictures(self):
+# 		print("We took so many pictures!")
+
+# thisWeekend = Friday("Movie", "Charlotte")
+
+# thisWeekend.money = 20
+
+# thisWeekend.friend = "Shane"
+
+# print(thisWeekend)
+# print(thisWeekend.money)
+# print(thisWeekend.friend)
+
+# Challenge 2
+# class Shopping:
+   
+# 	def __init__(self, item, quality):
+# 		self.item = item
+# 		self.quality = quality 
+# 		self.total = [] 
+		
+# 	def spending(self, cost):
+# 		self.total.append(cost)
+# sportStore = Shopping("Kayak", "High Quality")
+
+# sportStore.spending(130)
+# sportStore.spending(100)
+# sportStore.spending(120)
+
+# print(sportStore.total)
