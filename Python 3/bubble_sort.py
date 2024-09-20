9a510f89e1bfda730b5925e080375bdd93b75124
ashleymalmeida <almeida20082017@gmail.com>
2024-09-20 12:33:41 -0700

    exported from Skill Struck

---
+++ Python 3/bubble_sort.py
@@ -0,0 +1,24 @@
+# CHECKPOINT
+# arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+
+# reply_answer = int(input("Choose a number"))
+
+# arr.append(reply_answer)
+
+# def bubbleSort(sort_list):
+#     n = len(sort_list)
+#     for i in range(n-1):
+#         for j in range(0, n-i-1):
+#             if sort_list[j] > sort_list[j + 1]:
+#                 sort_list[j], sort_list[j + 1] = sort_list[j + 1], sort_list[j]
+#     return sort_list
+
+
+# bubbleSort(arr)
+
+# print(arr)
+
+
+
+
+           
