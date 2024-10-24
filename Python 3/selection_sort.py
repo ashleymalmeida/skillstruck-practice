a7ca59617c7c43f982d8af77a30bf3daf81c45d5
ashleymalmeida <almeida20082017@gmail.com>
2024-10-24 12:38:24 -0700

    exported from Skill Struck

--- Python 3/selection_sort.py
+++ Python 3/selection_sort.py
@@ -1,5 +1,18 @@
-for i in range(len(sort_list)):
-        smaller_index = i
-        for j in range(i+1, len(sort_list)):
-            if sort_list[smaller_index] > sort_list[j]:
-                smaller_index = j
+# CHECKPOINT
+# def selection_sort(sort_list):
+#     for i in range(len(sort_list)):
+#         smaller_index = i
+#         for j in range(i+1, len(sort_list)):
+#             if sort_list[smaller_index] > sort_list[j]:
+#                 smaller_index = j
+#         sort_list[smaller_index], sort_list[i] = sort_list[i], sort_list[smaller_index]
+#     return sort_list
+
+# arr = [10, 32, 4, 90, 15, 20, 89, 1, 3, 45, 42, 87, 91, 18, 25, 76, 38, 12]
+
+# user_answer = int(input("Enter a number: "))
+# arr.append(user_answer)
+
+# selection_sort(arr)
+
+# print(arr)
