ec55cb9296ee72b1afb567e29587dc1294f3a0d2
ashleymalmeida <almeida20082017@gmail.com>
2024-10-24 12:38:23 -0700

    exported from Skill Struck

---
+++ Python 3/recursion_vs_iteration.py
@@ -0,0 +1,27 @@
+# CHECKPOINT
+# question = int(input("Insert a number"))
+# def factorial(n): 
+# 	if n <= 1 :
+# 		return 1
+# 	else:
+# 		return (n * factorial(n - 1)) 
+
+# print(factorial(question))
+
+# Challenge 1
+# list_of_nums = [int(n) for n in input().split()]
+# def add_list(my_list):
+# 	if len(my_list) <= 1:
+# 		return my_list[0]
+# 	else:
+# 		return my_list[0] + add_list(my_list[1:])
+
+# print(add_list(list_of_nums))
+
+
+
+
+
+
+
+
