75b74833e6906634fcb1bda8e6ab322b9196dcd3
ashleymalmeida <almeida20082017@gmail.com>
2024-10-24 12:38:29 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/python_datetime.py
@@ -0,0 +1,48 @@
+# import datetime
+# x = datetime.datetime.now()
+# today = datetime.date.today()
+
+# print(x)
+# print(today)
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.strftime("%X"))
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.strftime("%m"))
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.strftime("%d"))
+
+# import datetime
+# x = datetime.datetime.now()
+# print(x.strftime("%Y"))
+
+
+
+# thought = input("How many months?")
+# idea = input("How many days?")
+
+
+# import datetime
+
+# x = datetime.datetime.now()
+
+# month = x.strftime("%m")
+# day = x.strftime("%d")
+
+# print("It has been " + month + " months and " + day + " days since your New Years resolution. How are you doing?")
+
+# Challenge 2
+# import datetime
+
+# x = datetime.datetime.now()
+
+# year = x.strftime("%Y")
+
+# years_passed = int(year) - 1972
+
+# print("It has been " + str(years_passed) + " years since the first computer program ever. Look how far we have come!")
