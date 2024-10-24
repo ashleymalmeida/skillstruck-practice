3e7914479ba62ce54ff34031fd73e329deb9cd71
ashleymalmeida <almeida20082017@gmail.com>
2024-10-24 12:38:31 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/error_handling.py
@@ -0,0 +1,42 @@
+# checkpoint
+# greeting = "Welcome"
+
+# try:
+#     print(greeting)
+# except:
+#     print("A problem has occured")
+# else:
+#     print("The code work without an error")
+# finally:
+#     print("It was accomplished")
+
+# Challenge 1
+# q1 = input("How many slices of pizzas?")
+# q2 = input("How many people will be at the party?")
+
+# try:
+#     slices_per_person = q1 / q2
+#     print("Each person will recieve " + slices_per_person + " slices of pizza")
+# except:
+#     print("Your code doesn't account for if a user tries to enter 0 people")
+# else:
+#     print("There is no problem")
+# finally:
+#     print("Your exception handling is complete")
+
+
+
+# Challenge 2
+# seeds_per_flower = 300
+
+# think1 = input("How many numbers of sunflowers?")
+
+# try:
+#     number_of_seeds = think1 * seeds_per_flower
+#     print("The total of sunflower seeds is" + number_of_seeds)
+# except:
+#     print("There is a problem")
+# else:
+#     print("There is no problem")
+# finally:
+#     print("Your exception handling is complete")
