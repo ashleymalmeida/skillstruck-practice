bfa8dbab20a4a5eeffe760e938848d55e846fc3f
ashleymalmeida <almeida20082017@gmail.com>
2024-10-24 12:38:28 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/python_math_library.py
@@ -0,0 +1,33 @@
+# CHECKPOINT
+# import math
+# x = math.ceil(4.2)
+# print(x)
+
+# import math
+# x = math.floor(4.2)
+# print(x)
+
+# import math
+# x = math.sqrt(900)
+# print(x)
+
+# import math
+# x = math.pow(3,4)
+# print(x)
+
+# Challenge 1 
+# import math
+
+# q1 = int(input("Number of people in town"))
+# q2 = int(input("Number of bikes in the town"))
+
+# people_per_town = math.ceil(q1/q2)
+# print("In this town, for every bike that exists there are " + str(people_per_town) + " people.")
+
+
+# Challenge 2
+# import math
+# length = int(input("Enter the length of the cube: "))
+
+# print(pow(length, 3))
+
