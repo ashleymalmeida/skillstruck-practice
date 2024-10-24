2d8ab48c9be55d311d48469e00ac7dc2d26e47c7
ashleymalmeida <almeida20082017@gmail.com>
2024-10-24 12:38:30 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/printing_specific_in_python.py
@@ -0,0 +1,16 @@
+# Checkpoint
+# print("I like \n to sleep")
+# print("I like \t to sleep")
+# print("I like \r to sleep")
+
+
+# Challenge 1 
+# print("Government History \t     12")
+# print("Evironmental science \t  11")
+# print("Geometry \t       10")
+# print("English \t   9")
+
+# Challenge 2
+# print("Once \n upon \n a \n time")
+
+
