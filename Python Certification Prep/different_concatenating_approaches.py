cb0f93439f2974c2e9466b632ada1d4d19e6d7b7
ashleymalmeida <almeida20082017@gmail.com>
2024-10-24 12:38:31 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/different_concatenating_approaches.py
@@ -0,0 +1,35 @@
+# Checkpoint
+# name = "Ashley"
+# dogs = 3
+# location = "Town"
+
+# print("My name is " + name  + " and I have " +  str(dogs) + " dogs and live in " + location)
+
+# print(f"My name is {name} and I have {dogs} dogs and live in {location}")
+
+# print("My name is" , name , "and I have" , dogs , "dogs and live in" , location)
+
+
+# Challenge 1 
+# v1 = "Carrots"
+# v2 = "Cauliflower"
+# v3 = "Pumpkin"
+# v4 = "Tomatoes"
+# v5 = "Cucumbers"
+
+# n1 = 4
+# n2 = 2
+# n3 = 7
+# n4 = 1
+# n5 = 6
+
+# print(f"Today I went to the market and I collected {n1} of {v1}, {n2} of {v2}, {n3} of {v3}, {n4} of {v4}, and {n5} of {v5}. Which is just enough to prepare dinner!")
+
+# Challenge 2 
+# baseball = 30
+# basketball = 70
+# volleyball = 25
+# soccer = 5
+# tennis = 13
+
+# print(baseball,basketball,volleyball,soccer,tennis)
