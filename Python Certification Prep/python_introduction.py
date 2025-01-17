b368c3e6de250a644b1fabc445c57d7f2fd8dd4f
ashleymalmeida <almeida20082017@gmail.com>
2025-01-17 12:25:26 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/python_introduction.py
@@ -0,0 +1,39 @@
+# Checkpoint
+# print("Hello World")
+
+#Challenges
+# print("Hello World")
+# print("I love to code!")
+
+
+# print("Hello World")
+# print("I love to code!")
+# print("My favorite color is blue")
+# print("My favorite color is black")
+# print("My favorite color is purple")
+
+
+# print("Hello World")
+# print("I love to code!")
+# print("My favorite color is blue")
+# print("My favorite color is black")
+# print("My favorite color is purple")
+# print("My favorite food is enchiladas")
+# print("My favorite side is corn")
+# print("My favorite drink is Hic fruit punch")
+
+
+# print("Hello World")
+# print("I love to code!")
+# print("My favorite color is blue")
+# print("My favorite color is black")
+# print("My favorite color is purple")
+# print("My favorite food is enchiladas")
+# print("My favorite side is corn")
+# print("My favorite drink is Hic fruit punch")
+# print("I like to draw")
+# print("I like to paint")
+# print("I like to dance")
+# print("I like to play")
+# print("I like to clean")
+
