fb398ce7e0480e57b382b5a98082a9af4de9fceb
ashleymalmeida <almeida20082017@gmail.com>
2025-01-17 12:25:29 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/syntax_and_comments_.py
@@ -0,0 +1,23 @@
+#Checkpoint
+# This is my comment
+# word = "Do you see it?"
+#This is another code
+
+#Challenges
+# color = "I feel grey"
+# It was a boring day
+# sound = "It sounds alright"
+# It is kind of loud
+# flavor = "really plain"
+# Nothing special on the menu
+
+
+# parent1 = "Hi"
+# There is people near by
+# parent2 = "What did you just said?"
+# Want to hear it againg if they misunderstood the person
+# parent3 = "Whatever"
+# They don't care about anything
+# print(parent1)
+# print(parent2)
+# print(parent3)
