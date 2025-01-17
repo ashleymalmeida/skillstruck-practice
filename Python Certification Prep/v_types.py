255b159c685d2e6ba335161e1e507470012f8e44
ashleymalmeida <almeida20082017@gmail.com>
2025-01-17 12:25:30 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/v_types.py
@@ -0,0 +1,27 @@
+#Checkpoint
+# name = "Peter"
+# name2 = 'Peter'
+# print(name)
+# print(name2)
+
+# num = 2
+# print(num)
+
+# n1 = True
+# n2 = False
+# print(n1)
+# print(n2)
+
+
+#Challenges
+# boo = True
+# lost = False
+
+# number = 42
+# greeting = "Hello!"
+# wonder = True
+# print(number)
+# print(greeting)
+# print(wonder)
+
+
