8ba2b65a441a9ea6a218ddad3d69671b6a78f556
ashleymalmeida <almeida20082017@gmail.com>
2025-01-17 12:25:28 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/input_output_.py
@@ -0,0 +1,28 @@
+#Checkpoint
+# question = input("What is your favorite color?")
+
+# print("Your favorite color is " + question)
+
+# Challenges
+# name = input("What is your name?")
+# print("Hello, " + name + '!')
+
+
+# ice_cream_flavor = input("What is your favorite flavor?")
+# mix_in = input("What treat do you add?") 
+# topping = input("What toppings do you put?")
+
+# print(ice_cream_flavor)
+# print(mix_in)
+# print(topping)
+
+
+# q1 = input("What do you like to do?")
+# q2 = input("What's your favrite movie?")
+# q3 = input("When is the BEST time of day?")
+# q4 = input("What pet peeves do you have?")
+
+# print(q1)
+# print(q2)
+# print(q3)
+# print(q4)
