6f61de233d029745582c2db0c40f03d58ed4740e
ashleymalmeida <almeida20082017@gmail.com>
2025-01-17 12:25:27 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/v_ariables.py
@@ -0,0 +1,36 @@
+#Checkpoint
+# treat = "Ice cream"
+# flavor = "Chocolate"
+# location = "Downtown"
+
+# print(treat)
+# print(flavor)
+# print(location)
+
+#Challenges
+
+# main_course = "Spaghetti with meatballs"
+# side = "corn"
+# drink = "Coca Cola"
+
+# print(main_course)
+# print(side)
+# print(drink)
+
+
+# basketball = 13
+# people = 5
+# hamburgers = 1,200
+
+# print(basketball)
+# print(people)
+# print(hamburgers)
+
+
+# hero1 = "Mom"
+# hero2 = "Dad"
+# hero3 = "Me"
+
+# print(hero1)
+# print(hero2)
+# print(hero3)
