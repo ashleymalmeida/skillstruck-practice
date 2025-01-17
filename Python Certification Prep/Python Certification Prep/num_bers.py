703930eec70ad4667a6c67900f8c1dbee1e84e6a
ashleymalmeida <almeida20082017@gmail.com>
2025-01-17 12:25:30 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/Python Certification Prep/num_bers.py
@@ -0,0 +1,17 @@
+#Checkpoint
+# student1 = 42
+# student2 = 4.3
+
+#Challenges
+# feet_in_meters = 3.2
+# print(feet_in_meters)
+
+# rank_outdoors = 9
+# print(rank_outdoors)  
+# rank_indoors = 10
+# print(rank_indoors)
+# time_outdoors = 2.0
+# print(time_outdoors)
+# time_indoors = 10.0
+# print(time_indoors)
+
