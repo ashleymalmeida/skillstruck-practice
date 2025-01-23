8a785874a03a21e63abae9ddfb0d009c73a8e213
ashleymalmeida <almeida20082017@gmail.com>
2025-01-23 12:32:19 -0700

    exported from Skill Struck

---
+++ Python Certification Prep/me.py
@@ -0,0 +1,14 @@
+# number = 1
+# while number <= 50:
+# 	print(number)
+# 	number += 1
+
+
+my_number = int(input("Pick a number."))
+largest = my_number
+while my_number != 0:
+	my_number = 
+	if my_number > :
+		largest = 
+
+print()
